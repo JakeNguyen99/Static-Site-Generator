1e12028b5499bc9f41af48d4c617d38a02aba4ac
Nguyen Viet Quan <quannguyenviet99@gmail.com>
2024-07-10 16:41:23 +0700

    add text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,7 @@ from textnode import (
     text_type_code,
     text_type_image,
     text_type_link,
+    text_node_to_html_node,
 )
 
 
@@ -39,6 +40,42 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", text_type_text)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", text_type_image, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", text_type_bold)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
+
+    def test_code(self):
+        node = TextNode("this is code", text_type_code)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "this is code")
+
+    def test_link(self):
+        node = TextNode("this is a link", text_type_link, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "this is a link")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
 
 
 if __name__ == "__main__":
