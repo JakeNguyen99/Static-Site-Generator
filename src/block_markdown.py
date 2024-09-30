9633ca8083beb54f0a2b23d0917933877dddde2a
Nguyen Viet Quan <quannguyenviet99@gmail.com>
2024-09-30 10:51:25 +0700

    add block_to_block_type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -4,3 +4,21 @@ def markdown_to_blocks(markdown):
   blocks = re.split(r'\n\s*\n', markdown.strip())
   return blocks
 
+def block_to_block_type(block):
+  if re.match(r'^#{1,6} ', block):
+    return 'heading'
+  
+  if re.match(r'^```', block):
+    return 'code'
+  
+  if re.match(r'^> ', block):
+    return 'quote'
+  
+  if re.match(r'^\* ', block) or re.match(r'^\- ', block):
+    return 'unordered_list'
+  
+  if re.match(r'^\d{1}. ', block):
+    return'ordered_list'
+  
+  return 'paragraph'
+  
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,11 +1,12 @@
 import unittest
 
 from block_markdown import(
-  markdown_to_blocks
+  markdown_to_blocks,
+  block_to_block_type
 )
 
 class TestBlockMardown(unittest.TestCase):
-  def test_markdown_to_blocks(self):
+   def test_markdown_to_blocks(self):
      markdown = """# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"""
      blocks = markdown_to_blocks(markdown)
      self.assertListEqual(
@@ -16,6 +17,27 @@ class TestBlockMardown(unittest.TestCase):
         ],
         blocks,
      )
+
+   def test_heading(self):
+     self.assertEqual(block_to_block_type("# This is a heading"), "heading")
+   
+   def test_code(self):
+      self.assertEqual(block_to_block_type("```code```"), "code")
+
+   def test_quote(self):
+      self.assertEqual(block_to_block_type("> quote"), "quote")
+
+   def test_unordered_list_1(self):
+      self.assertEqual(block_to_block_type("* unorder list 1"), "unordered_list")
+
+   def test_unordered_list_2(self):
+      self.assertEqual(block_to_block_type("- unordered list 2"), "unordered_list")
+
+   def test_ordered_list_2(self):
+      self.assertEqual(block_to_block_type("1. ordered list"), "ordered_list")
+
+   def test_paragraph(self):
+      self.assertEqual(block_to_block_type("This is a paragraph of text. It has some **bold** and *italic* words inside of it."), "paragraph")
 
 if __name__ == "__main__":
     unittest.main()
